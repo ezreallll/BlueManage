bf6e28a471a4a57d2e7a63e82b6f525b84c76241
shp <330733768@qq.com>
2017-02-16 16:28:41 +0800

    index

--- js/common.js
+++ js/common.js
@@ -4,16 +4,25 @@
 require.config({
     baseUrl: "js/app",
     paths:{
-        "jquery":'../../lib/jquery-1.11.1.min',
-        "bootstrap":'../../lib/bootstrap.min',
+        "jquery":'../../lib/jquery.min',
+        "bootstrap":'../../lib/bootstrap',
         "chart":'../../lib/chart.min',
         "chart-data":'../../lib/chart-data',
         "easypiechart":'../../lib/easypiechart',
         "easypiechart-data":'../../lib/easypiechart-data',
-        "bootstrap-datepicker":'../../lib/bootstrap-datepicker',
+        "bootstrap-datepicker":'../../lib/bootstrap-datepicker'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery']
+        },
+        'easypiechart-data': {
+            deps: ['jquery']
+        },
+        'bootstrap-datepicker': {
+            deps: ['jquery']
+        }
     }
-
-}
-)
+});
 
 
